507ee7c6a950959cd5890bb65afcdd49f94ed1c2
shabaj <shabajansari843@gmail.com>
2021-11-09 16:27:44 +0530

    Completed Music Player

---
+++ RealmDB/app/src/main/java/com/asi/realmdb/Notes.java
@@ -0,0 +1,2 @@
+package com.asi.realmdb;public class Notes {
+}
---
+++ RealmDB/app/src/main/java/com/asi/realmdb/addnote.java
@@ -0,0 +1,14 @@
+package com.asi.realmdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addnote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addnote);
+    }
+}
